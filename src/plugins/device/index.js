81d570504e4a84f1b2ad477c51d5ff2a4d1a5130
synder <synder@synders-MacBook-Pro.local>
2017-07-26 18:41:23 +0800

    first commit

--- src/plugins/device/index.js
+++ src/plugins/device/index.js
@@ -4,25 +4,50 @@
  * @desc
  */
 
+const android = function (ua) {
+    ua = ua || navigator.userAgent;
+    ua.match(/(Android);?[\s\/]+([\d.]+)?/)
+};
 
-const webkit = ua.match(/Web[kK]it[\/]{0,1}([\d.]+)/);
-const android = ua.match(/(Android);?[\s\/]+([\d.]+)?/);
-const osx = !!ua.match(/\(Macintosh\; Intel /);
-const ipad = ua.match(/(iPad).*OS\s([\d_]+)/);
-const ipod = ua.match(/(iPod)(.*OS\s([\d_]+))?/);
-const iphone = !ipad && ua.match(/(iPhone\sOS)\s([\d_]+)/);
-const webos = ua.match(/(webOS|hpwOS)[\s\/]([\d.]+)/);
-const wp = ua.match(/Windows Phone ([\d.]+)/);
-const touchpad = webos && ua.match(/TouchPad/);
-const kindle = ua.match(/Kindle\/([\d.]+)/);
-const silk = ua.match(/Silk\/([\d._]+)/);
-const blackberry = ua.match(/(BlackBerry).*Version\/([\d.]+)/);
-const bb10 = ua.match(/(BB10).*Version\/([\d.]+)/);
-const rimtabletos = ua.match(/(RIM\sTablet\sOS)\s([\d.]+)/);
-const playbook = ua.match(/PlayBook/);
-const chrome = ua.match(/Chrome\/([\d.]+)/) || ua.match(/CriOS\/([\d.]+)/);
-const firefox = ua.match(/Firefox\/([\d.]+)/);
-const firefoxos = ua.match(/\((?:Mobile|Tablet); rv:([\d.]+)\).*Firefox\/[\d.]+/);
-const ie = ua.match(/MSIE\s([\d.]+)/) || ua.match(/Trident\/[\d](?=[^\?]+).*rv:([0-9.].)/);
-const webview = !chrome && ua.match(/(iPhone|iPod|iPad).*AppleWebKit(?!.*Safari)/);
-const safari = webview || ua.match(/Version\/([\d.]+)([^S](Safari)|[^M]*(Mobile)[^S]*(Safari))/);
+const wechat = function (ua) {
+    ua = ua || navigator.userAgent;
+    return /micromessenger/i.test(ua);
+};
+
+const ios = function (ua) {
+    ua = ua || navigator.userAgent;
+    return ua.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)
+};
+
+const wp = function (ua) {
+    ua = ua || navigator.userAgent;
+    ua.match(/Windows Phone ([\d.]+)/)
+};
+
+const chrome = function (ua) {
+    ua = ua || navigator.userAgent;
+    ua.match(/Chrome\/([\d.]+)/) || ua.match(/CriOS\/([\d.]+)/)
+};
+
+const firefox = function (ua) {
+    ua = ua || navigator.userAgent;
+    ua.match(/Firefox\/([\d.]+)/)
+};
+
+const ie = function (ua) {
+    ua = ua || navigator.userAgent;
+    ua.match(/MSIE\s([\d.]+)/) || ua.match(/Trident\/[\d](?=[^\?]+).*rv:([0-9.].)/)
+};
+
+export {wechat, ios, android, wp, chrome, firefox, ie};
+
+export default{
+    install (Vue) {
+        Vue.prototype.$device = {
+            wp: wp,
+            ios: ios,
+            android: android,
+            wechat: wechat,
+        };
+    }
+}
--- src/plugins/repair/prototype.js
+++ src/plugins/repair/prototype.js
@@ -4,35 +4,37 @@
  * @desc
  */
 
-if (String.prototype.trim === undefined) // fix for iOS 3.2
+// fix for iOS 3.2
+if (String.prototype.trim === undefined) {
     String.prototype.trim = function () {
-        return this.replace(/^\s+|\s+$/g, '')
+        return this.replace(/^\s+|\s+$/g, '');
     };
+}
 
 // For iOS 3.x
-if (Array.prototype.reduce === undefined){
+if (Array.prototype.reduce === undefined) {
     Array.prototype.reduce = function (fun) {
-        if (this === void 0 || this === null) throw new TypeError()
-        var t = Object(this), len = t.length >>> 0, k = 0, accumulator
-        if (typeof fun != 'function') throw new TypeError()
-        if (len == 0 && arguments.length == 1) throw new TypeError()
+        if (this === void 0 || this === null) throw new TypeError();
+        var t = Object(this), len = t.length >>> 0, k = 0, accumulator;
+        if (typeof fun !== 'function') throw new TypeError();
+        if (len === 0 && arguments.length === 1) throw new TypeError();
 
-        if (arguments.length >= 2)
-            accumulator = arguments[1]
-        else
+        if (arguments.length >= 2){
+            accumulator = arguments[1];
+        } else {
             do {
                 if (k in t) {
-                    accumulator = t[k++]
+                    accumulator = t[k++];
                     break
                 }
                 if (++k >= len) throw new TypeError()
-            } while (true)
+            } while (true);
+        }
 
         while (k < len) {
-            if (k in t) accumulator = fun.call(undefined, accumulator, t[k], k, t)
-            k++
+            if (k in t) accumulator = fun.call(undefined, accumulator, t[k], k, t);
+            k++;
         }
-        return accumulator
-    }
+        return accumulator;
+    };
 }
-    
